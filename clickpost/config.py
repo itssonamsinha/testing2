1499c961aba8c5aaaaf4ebe3f7274415a2e20262
Sonam Sinha <sonamsinha@policybazaar.com>
2020-03-17 16:54:25 +0530

    fixes

--- clickpost/config.py
+++ clickpost/config.py
@@ -7,18 +7,16 @@ class SendMessages:
 
     #  we are calling this method for all the entries whose messages have not been sent yet
 
-    def send_notification_message(self, user_id, status, shipmentid):
+    def send_notification_message(self, user_id, status):
         records_obj = StatusNotificationTypeRecords.objects.filter(user_id=user_id, status__exact=status, is_sent=False)
-        try:
-            for data in records_obj:
-                # check if user is authorized or not (raise error if not)
-                if data.notification_type == NotificationAction.SMS_NOTIFICATION:
-                    obj = NotificationAction.SMS_NOTIFICATION()
-                elif data.notification_type == NotificationAction.WHATSAPP_Notification:
-                    obj = NotificationAction.WHATSAPP_Notification()
 
-                obj.send(data.user, data.shipment_id, data.status)
-                data.is_sent = True
-                data.save()
-        except Exception as e:
-            raise Exception(e)
+        for data in records_obj:
+            # check if user is authorized or not (raise error if not)
+            if data.notification_type == NotificationAction.SMS_NOTIFICATION:
+                obj = NotificationAction.SMS_NOTIFICATION()
+            elif data.notification_type == NotificationAction.WHATSAPP_Notification:
+                obj = NotificationAction.WHATSAPP_Notification()
+
+            obj.send(data.user, data.shipment_id, data.status)
+            data.is_sent = True
+            data.save()
--- clickpost/views.py
+++ clickpost/views.py
@@ -2,7 +2,7 @@ from requests import Response
 from rest_framework import viewsets
 from rest_framework import status
 from clickpost.config import SendMessages
-from clickpost.models import User
+from clickpost.models import User, StatusNotificationTypeRecords
 
 
 class NotificationView(viewsets.GenericViewSet):
@@ -23,14 +23,19 @@ class NotificationView(viewsets.GenericViewSet):
         user_id = request.GET.get('user_id', None)
         status = request.GET.get('status', None)
         shipment_id = request.GET.get('shipment_id', None)
-        if not user_id or not status or not shipment_id:
-            return Response({'success': False, 'message': 'user id or status or shipment id is missing'})
+        notification_type = request.GET.get('notification_type', None)
+        if not user_id or not status or not shipment_id or not notification_type:
+            return Response({'success': False, 'message': 'user id or status or shipment id  or notification_type is missing'})
 
-        if user_id:
-            user = User.objects.filter(id=user_id).first()
-            if not user or user.is_verified == False:
-                return Response({'success': False, 'message': 'User not verified'})
+        user = User.objects.filter(id=user_id).first()
+        if not user or user.is_verified == False:
+            return Response({'success': False, 'message': 'User not verified'})
 
-        SendMessages.send_notification_message(user_id, status, shipment_id)
+        try:
+            StatusNotificationTypeRecords.objects.create(user_id=user_id, status=status, notification_type=notification_type)
+            SendMessages.send_notification_message(user_id, status)
+        except Exception as e:
+            raise Exception(e)
+
 
 
